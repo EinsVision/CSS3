5e1c333ae1253a8ebbd9dc5e740af996e7fc2fdd
EinsVision <63795065+EinsVision@users.noreply.github.com>
2020-08-29 23:27:37 +0900

    Object Model

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var hw = document.getElementById('hw');
         hw.addEventListener('click', function(){
             alert('Hello World');
-        })
+        });
+
+var image = document.getElementsByTagName("img");
+image[0].style.width('50px');
